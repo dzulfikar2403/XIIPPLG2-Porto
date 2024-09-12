4eeed721d4d21f7547b0a9149ecc9127193a0652
dzulfikar2403 <dzulfikar2403@gmail.com>
2024-09-12 11:13:39 +0700

    add origin cors

--- client/src/pages/SiswaPage.jsx
+++ client/src/pages/SiswaPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import GridLayout from '../components/layout/GridLayout'
 import CardSiswa from '../components/fragment/CardSiswa';
+import { useSelector } from 'react-redux';
 
 const SiswaPage = () => { 
   const { dataSiswa } = useSelector((state) => state.siswaSlice);
